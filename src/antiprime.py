79ae0a029f5138991419df7a3e1056940c0f4a0b
Gabriela Perdomo <gabrielasofia.perdomo01@estudiant.upf.edu>
2024-10-21 09:39:23 +0200

    First attempt

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,24 +1,34 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
+import sys
+x = int(sys.argv[1])
 def main() :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
-
-
-	## THE LAST LINES OF YOUR CODE SHOULD EITHER
-	## RETURN THE VALUE "anti-prime" or "not anti-prime"
-	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
-	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
-	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
+	i = 1
+	j = 1
+	y = x - 1
+	a = 0
+	d = 0
+	while (i <= x):
+		if (x % i == 0):
+			d = d + 1
+		i = i + 1
+	while (y < x and y > 1):	
+		while (j <= y):
+			if (y % j == 0):
+				a = a + 1
+			j = j + 1
+		if (a >= d):
+			return("no anti-prime")
+		else:
+			y = y - 1
+			j = 1
+			a = 0
+	if (a < d):
+		return("anti-prime")
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
-
-	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
-	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
-	## $ python antiprime.py 6
-	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
-	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
 	print(main())
